3235e667f09e23955990a8f3651d9676a74d6cb3
Iriadna777 <iriadna777@gmail.com>
2024-04-28 01:13:59 +0200

    add task 7

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.querySelector('#name-input');
+    const output = document.querySelector('#name-output');
+  
+    input.addEventListener('input', () => {
+      const trimmedInput = input.value.trim();
+      output.textContent = trimmedInput || 'Anonymous';
+    });
+  });
+  
---
+++ js/task-6.js
@@ -0,0 +1,44 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
+
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10; 
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  document.getElementById('boxes').innerHTML = '';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const input = document.querySelector('#controls input[type="number"]');
+  const createBtn = document.querySelector('button[data-create]');
+  const destroyBtn = document.querySelector('button[data-destroy]');
+
+  createBtn.addEventListener('click', () => {
+    const inputVal = input.value; 
+    const amount = parseInt(inputVal, 10); 
+
+    if (amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+    } else {
+      alert('Please enter a number between 1 and 100.');
+    }
+
+    input.value = ''; 
+  });
+
+  destroyBtn.addEventListener('click', destroyBoxes);
+});
